b64ba17b5076910fead986b104d12dd3ebe6de59
RobStarek <60106783+RobStarek@users.noreply.github.com>
2025-04-04 14:16:54 +0200

    Create example_h5.py

---
+++ cnn-model/example_h5.py
@@ -0,0 +1,27 @@
+import os
+import tensorflow as tf
+import numpy as np
+import h5py
+#One should set the following environment variable if keras 3 is used:
+#`export TF_USE_LEGACY_KERAS=1` into console.")
+
+#Set paths to input and output
+INPUT = 'test_in.h5'
+OUTPUT = 'test_out.h5'
+
+if __name__ == '__main'__:
+  #Replace custom metrics with default one. For running the code, training helper function
+  #are irrelevant anyway.
+  customs = {'Custom_mse_conv_func' : tf.keras.losses.mse, 'Custom_mae_conv_func' : tf.keras.losses.mae}
+  #Load Keras model.
+  model = tf.keras.models.load_model('model_v3.h5', custom_objects = customs)
+  print(model.summary())
+  #Iterate over all datasets in input file and transform them
+  #using the neural network.
+  with h5py.File(INPUT, 'r') as h5fi, h5py.File(OUTPUT, 'w') as h5fo:
+      for dsetkey in h5fi.keys():
+          print(dsetkey, h5fi[dsetkey].shape, h5fi[dsetkey].dtype)
+          images = np.array(h5fi[dsetkey])
+          transformed = model.predict(images)
+          h5fo.create_dataset(dsetkey, data = transformed)
+  print('done')
