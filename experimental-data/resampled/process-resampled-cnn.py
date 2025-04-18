252023bb167382953acb73c7f982beb97a73cedb
RobStarek <starek.robert@gmail.com>
2025-04-19 00:05:32 +0200

    Monte-Carlo/bootstrapping/resampling code.
    * Added some code for resampling (to estim. the statistical error.)
    * Fixed a typo in Figure2_Table1.ipynb.
    * Added images we used to extract background.
    * Added results from resampled MEF.

--- experimental-data/resampled/process-resampled-cnn.py
+++ experimental-data/resampled/process-resampled-cnn.py
@@ -1,2 +1,32 @@
-#TODO: put resampled images through CNN
-#and do it also for RL and save MEFs outputs
+"""
+Process generated Monte-Carlo images using our CNN model.
+"""
+
+import tensorflow as tf
+import numpy as np
+import h5py
+# One should set the following environment variable if keras 3 is used:
+# `export TF_USE_LEGACY_KERAS=1` into console.")
+
+# Set paths to input and output
+INPUTS = [f'mc_img{i}.h5' for i in (1, 2, 3)]
+OUTPUTS = [f'mc_img{i}_cnn.h5' for i in (1, 2, 3)]
+
+if __name__ == '__main__':
+    # Replace custom metrics with default one. For running the code, training helper function
+    # are irrelevant anyway.
+    customs = {'Custom_mse_conv_func': tf.keras.losses.mse,
+               'Custom_mae_conv_func': tf.keras.losses.mae}
+    # Load Keras model.
+    model = tf.keras.models.load_model('../../cnn-model/model.h5', custom_objects=customs)
+    print(model.summary())
+    # Iterate over all datasets in input file and transform them
+    # using the neural network.
+    for file_in, file_out in zip(INPUTS, OUTPUTS):
+        with h5py.File(file_in, 'r') as h5fi, h5py.File(file_out, 'w') as h5fo:
+            for dsetkey in h5fi.keys():
+                print(dsetkey, h5fi[dsetkey].shape, h5fi[dsetkey].dtype)
+                images = np.array(h5fi[dsetkey])
+                transformed = model.predict(images)
+                h5fo.create_dataset(dsetkey, data=transformed)
+    print('done')
--- experimental-data/resampled/process-resampled-rl.py
+++ experimental-data/resampled/process-resampled-rl.py
@@ -1,1 +1,60 @@
-#put all resampled images through RL algo
+"""
+Process resampled experimental images using Richardson-Lucy algorithm.
+"""
+
+import numpy as np
+from rl_module import Gauss_kernel, RL_iteration_for_concurrent
+from concurrent.futures import ProcessPoolExecutor
+import cv2
+import h5py
+
+
+
+INPUTS = [f'mc_img{i}.h5' for i in (1, 2, 3)]
+OUTPUTS = [f'mc_img{i}_rl.h5' for i in (1, 2, 3)]
+M = 17 #~ 3.*4*sqrt(2)
+KERNEL_X = Gauss_kernel(M)
+ITERS = 100e3
+CPU_units_to_use = 20 #rewrite according to your needs
+
+def norm(img):
+    im = img - np.min(img)
+    return im/np.sum(im)
+
+def rl_func(img_pair):    
+    """
+    Applies the Richardson-Lucy (RL) deconvolution algorithm to an up-sized image.
+
+    Args:
+        img_pair (tuple): A tuple containing:
+            - index (int): The index of the image.
+            - img (numpy.ndarray): The image to process.
+
+    Returns:
+        numpy.ndarray: The deconvolved image after applying the RL algorithm.
+
+    Notes:
+        - The function prints a message indicating the index of the image being processed.
+        - The image is resized to 200x200 using cubic interpolation before processing.
+        - The RL algorithm is applied with a maximum number of iterations (`max_iter_`) 
+          and a minimum change threshold (`min_change_`).
+    """
+    index, img = img_pair
+    print(f"calling RL @img{index}...")
+    return RL_iteration_for_concurrent(
+        KERNEL_X, 
+        norm(cv2.resize(img, (200,200), interpolation=cv2.INTER_CUBIC)),
+        max_iter_=ITERS, min_change_= 1e-10)    
+
+
+
+if __name__ == '__main__':
+    for file_in, file_out in zip(INPUTS, OUTPUTS):
+        with h5py.File(file_in, 'r') as h5fi, h5py.File(file_out, 'w') as h5fo:
+            for key in list(h5fi.keys()):
+                print(key, "...")
+                imgs = ((i, img) for i, img in enumerate(h5fi[key]))
+                with ProcessPoolExecutor(CPU_units_to_use) as pool:
+                    timgs = np.array(list(pool.map(rl_func, imgs)))            
+                h5fo.create_dataset(key, data = timgs)
+        print("Done.")
